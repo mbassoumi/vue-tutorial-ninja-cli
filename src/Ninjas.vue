310c60e1a18de760bcc50022e7f5509721548e5d
majdbassoumi <mbbassoumi@gmail.com>
2018-10-13 21:48:15 +0300

    init project

---
+++ src/Ninjas.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Ninjas list</h1>
+    <ul>
+      <li v-for="ninja in ninjas">{{ninja}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        ninjas: ['Majd', 'Basem', 'Bassoumi']
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1{
+    color: green;
+  }
+</style>
