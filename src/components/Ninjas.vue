69885ead0dd409363f8735aefff4548abd0a2e03
majdbassoumi <mbbassoumi@gmail.com>
2018-10-14 20:30:18 +0300

    lifecycle hooks

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -1,51 +1,71 @@
 <template>
-<div id="ninjas">
-  <ul>
-    <li v-for="ninja in ninjas" @click="ninja.show = !ninja.show">
-      <h2>{{ninja.name}}</h2>
-      <h3 v-if="ninja.show">{{ninja.speciality}}</h3>
-    </li>
-  </ul>
-  <button @click="deleteNinja">Delete Ninja</button>
-</div>
+  <div id="ninjas">
+    <ul>
+      <li v-for="ninja in ninjas" @click="ninja.show = !ninja.show">
+        <h2>{{ninja.name}}</h2>
+        <h3 v-if="ninja.show">{{ninja.speciality}}</h3>
+      </li>
+    </ul>
+    <button @click="deleteNinja">Delete Ninja</button>
+  </div>
 </template>
 
 <script>
-    export default {
-      props:{
-        'ninjas':{
-          type: Array,
-          required: true
-        }
-      },
-      data(){
-        return{
-
-        }
-      },
-      methods: {
-        deleteNinja(){
-          this.ninjas.pop()
-        }
+  export default {
+    props: {
+      'ninjas': {
+        type: Array,
+        required: true
       }
-    }
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      deleteNinja() {
+        this.ninjas.pop()
+      }
+    },
+
+    //lifecycle hooks
+    beforeCreate(){
+      alert('beforeCreate');
+    },
+    created(){
+      alert('created');
+    },
+    beforeMount(){
+      alert('beforeMount');
+    },
+    mounted(){
+      alert('mounted');
+    },
+    beforeUpdate(){
+      alert('beforeUpdate');
+    },
+    updated(){
+      alert('updated');
+    },
+  }
 </script>
 
 <style scoped>
-  #ninjas{
+  #ninjas {
     width: 100%;
     max-width: 1200px;
     margin: 40px auto;
     padding: 0 20px;
     box-sizing: border-box;
   }
-  ul{
+
+  ul {
     display: flex;
     flex-wrap: wrap;
     list-style-type: none;
     padding: 0;
   }
-  li{
+
+  li {
     flex-grow: 1;
     flex-basis: 300px;
     text-align: center;
